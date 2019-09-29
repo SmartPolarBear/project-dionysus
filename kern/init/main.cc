856f8107830f68d2756b5ea02d77e0f8ade206d9
SmartPolarBear <clevercoolbear@outlook.com>
2019-09-29 23:10:19 +0800

    successfully booting

--- kern/init/main.cc
+++ kern/init/main.cc
@@ -2,13 +2,26 @@
  * @ Author: SmartPolarBear
  * @ Create Time: 2019-09-23 23:06:29
  * @ Modified by: SmartPolarBear
- * @ Modified time: 2019-09-24 23:27:48
+ * @ Modified time: 2019-09-29 23:10:12
  * @ Description: the entry point for kernel in C++
  */
 
 #include "sys/param.h"
 
-extern "C" [[noreturn]] void kmain() 
+#define VIDEO_START 0xb8000
+#define VGA_LIGHT_GRAY 7
+static void PrintString(const char *str)
 {
-    for(;;);
+    unsigned char *video = ((unsigned char *)VIDEO_START);
+    while (*str != '\0')
+    {
+        *(video++) = *str++;
+        *(video++) = VGA_LIGHT_GRAY;
+    }
+}
+
+extern "C" [[noreturn]] void kmain() {
+    PrintString("Hello World!");
+    for (;;)
+        ;
 }
