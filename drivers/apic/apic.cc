f61912b42ecd7f603613f3674f949adb24682366
SmartPolarBear <clevercoolbear@outlook.com>
2019-10-21 23:11:03 +0800

    implement the place  which requires panic

---
+++ drivers/apic/apic.cc
@@ -0,0 +1,6 @@
+#include "drivers/apic/apic.h"
+
+void apic::apic_init(void)
+{
+
+}
---
+++ include/drivers/apic/apic.h
@@ -0,0 +1,10 @@
+#if !defined(__INCLUDE_DRIVERS_APIC_H)
+#define __INCLUDE_DRIVERS_APIC_H
+
+
+namespace apic
+{
+void apic_init(void);
+} // namespace apic
+
+#endif // __INCLUDE_DRIVERS_APIC_H
