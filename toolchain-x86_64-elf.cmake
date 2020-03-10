874fb3936b8bbab6887bd180b52ba13f7b168478
SmartPolarBear <clevercoolbear@outlook.com>
2020-03-10 22:47:15 +0800

    ready to add first user prog

--- toolchain-x86_64-elf.cmake
+++ toolchain-x86_64-elf.cmake
@@ -3,7 +3,7 @@ SET(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(CMAKE_GLD_LINKER_NAME clang++ CACHE STRING "Name of the Binutils linker")
+set(CMAKE_GLD_LINKER_NAME clang++ CACHE STRING "Name of the clang++ linker")
 mark_as_advanced(CMAKE_GLD_LINKER_NAME)
 
 find_program(CMAKE_GLD_LINKER ${CMAKE_GLD_LINKER_NAME})
