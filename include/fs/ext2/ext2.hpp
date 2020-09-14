478a474a116f93db6ab8912d2705d61e27ad6aec
SmartPolarBear <clevercoolbear@outlook.com>
2020-09-14 22:19:46 +0800

    ext2_read_superblock

--- include/fs/ext2/ext2.hpp
+++ include/fs/ext2/ext2.hpp
@@ -1,10 +1,68 @@
 #pragma once
 
 #include "system/types.h"
+
 #include "fs/vfs/vfs.hpp"
 
 namespace file_system
 {
+	struct ext2_superblock
+	{
+		// Base superblock
+		uint32_t inode_count;
+		uint32_t block_count;
+		uint32_t reserved_blocks;
+		uint32_t free_blocks;
+		uint32_t free_inodes;
+		uint32_t superblock_no;
+		uint32_t log2_block_size;
+		uint32_t log2_frag_size;
+		uint32_t block_group_blocks;
+		uint32_t block_group_frags;
+		uint32_t block_group_inodes;
+		uint32_t mount_time;
+		uint32_t write_time;
+		uint16_t mount_count;
+		uint16_t fsck_mount_count;
+		uint16_t ext2_signature;
+		uint16_t state;
+		uint16_t error_behavior;
+		uint16_t version_minor;
+		uint32_t fsck_time;
+		uint32_t fsck_interval;
+		uint32_t os_id;
+		uint32_t version_major;
+		uint16_t reserved_uid;
+		uint16_t reserved_gid;
+		// Extended superblock
+		uint32_t first_inode;                   // == 11 in < 1.0
+		uint16_t inode_size;                    // == 128 in < 1.0
+		uint16_t superblock_block_group;        // For backup
+		uint32_t optional_features;             // Don't matter
+		uint32_t required_features;             // Cannot read without these
+		uint32_t write_required_features;       // Can read without these
+		uint8_t filesystem_id[16];
+		uint8_t volume_name[16];
+		uint8_t last_mount_path[64];
+		uint32_t compression;
+		uint8_t prealloc_file_blocks;
+		uint8_t prealloc_dir_blocks;
+		uint16_t __res0;
+		uint8_t journal_id[16];
+		uint32_t journal_inode;
+		uint32_t journal_device;
+		uint32_t orphan_inode_list_head;
+	} __attribute__((packed));
+
+	struct ext2_data
+	{
+		union
+		{
+			ext2_superblock block;
+			uint8_t block_data;
+		};
+	};
+
 	class ext2_fs_class
 		: public fs_class_base
 	{
--- kern/fs/ext2/ext2.cc
+++ kern/fs/ext2/ext2.cc
@@ -1,10 +1,29 @@
 #include "fs/ext2/ext2.hpp"
-
 #include "fs/vfs/vfs.hpp"
+
+#include "drivers/debug/kdebug.h"
 
 using namespace file_system;
 
 ext2_fs_class file_system::g_ext2fs;
+
+static inline error_code ext2_read_superblock(fs_instance* fs)
+{
+	if (fs->private_data == nullptr)
+	{
+		return -ERROR_INVALID;
+	}
+
+	ext2_data* data = reinterpret_cast<ext2_data*>(fs->private_data);
+
+	size_t read_size = fs->dev->read(reinterpret_cast<void*>(data->block_data), 1024, 1024);
+	if (read_size != 1024)
+	{
+		return -ERROR_IO;
+	}
+
+	return ERROR_SUCCESS;
+}
 
 file_system::vnode_base* file_system::ext2_fs_class::get_root(fs_instance* fs)
 {
@@ -13,6 +32,21 @@ file_system::vnode_base* file_system::ext2_fs_class::get_root(fs_instance* fs)
 
 error_code file_system::ext2_fs_class::initialize(fs_instance* fs, const char* data)
 {
+	fs->private_data = new ext2_data{};
+
+	if (fs->private_data == nullptr)
+	{
+		return -ERROR_MEMORY_ALLOC;
+	}
+
+	error_code ret = ext2_read_superblock(fs);
+	if (ret != ERROR_SUCCESS)
+	{
+		delete fs->private_data;
+		fs->private_data = nullptr;
+		return ret;
+	}
+
 	return 0;
 }
 
@@ -25,3 +59,4 @@ error_code file_system::ext2_fs_class::get_vfs_status(fs_instance* fs, vfs_statu
 {
 	return 0;
 }
+
