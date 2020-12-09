75ef81f9ed7f389b5e6adba680248d6f99afe639
SmartPolarBear <clevercoolbear@outlook.com>
2020-12-09 21:47:06 +0800

    Fix some error in allocate_inode, but it doesn't work neither

--- kern/fs/ext2/inode/inode.cc
+++ kern/fs/ext2/inode/inode.cc
@@ -73,8 +73,9 @@ error_code ext2_inode_write(file_system::fs_instance* fs,
 		return -ERROR_INVALID;
 	}
 
-	const size_t block_size = data->get_inodes_per_block();
+	const size_t block_size = data->get_block_size();
 	const size_t inode_size = data->get_inode_size();
+	const size_t inodes_per_block = data->get_inodes_per_block();
 
 	auto superblock = data->get_superblock();
 
@@ -84,12 +85,12 @@ error_code ext2_inode_write(file_system::fs_instance* fs,
 	}
 
 	auto inode_group = EXT2_INODE_GET_BLOCK_GROUP(inum, superblock.block_group_inode_count);
-	auto inode_group_index = EXT2_INODE_INDEX_IN_BLOCK_GROUP(inum, superblock.block_group_inode_count);
+	auto inode_index_in_group = EXT2_INODE_INDEX_IN_BLOCK_GROUP(inum, superblock.block_group_inode_count);
 
 	auto inode_block =
-		data->get_bgd_by_index(inode_group).inode_table_no + inode_group_index / data->get_inodes_per_block();
+		data->get_bgd_by_index(inode_group).inode_table_no + inode_index_in_group / inodes_per_block;
 
-	auto offset_in_block = (inode_group_index % data->get_inodes_per_block()) * data->get_inode_size();
+	auto offset_in_block = (inode_index_in_group % inodes_per_block) * inode_size;
 
 	if (offset_in_block >= block_size)
 	{
@@ -108,7 +109,7 @@ error_code ext2_inode_write(file_system::fs_instance* fs,
 		return ret;
 	}
 
-	memcpy(block_buf + offset_in_block, inode, inode_size);
+	memcpy(offset_in_block + block_buf, inode, inode_size);
 
 	if (auto ret = ext2_block_write(fs, block_buf, inode_block);ret != ERROR_SUCCESS)
 	{
@@ -131,66 +132,71 @@ error_code_with_result<uint32_t> ext2_inode_alloc(file_system::fs_instance* fs, 
 	}
 
 	uint64_t* bitmap_buf = new(std::nothrow)uint64_t[data->get_block_size() / sizeof(uint64_t)];
-
-	for (size_t i = 0; i < data->get_bgdt_entry_count(); i++)
+	uint8_t* bitmap_bytes = reinterpret_cast<uint8_t*>(bitmap_buf);
+	if (bitmap_buf == nullptr)
 	{
-		auto bgd = data->get_bgdt() + i;
-		memset(bitmap_buf, 0, data->get_block_size());
+		return -ERROR_MEMORY_ALLOC;
+	}
 
-		if (bgd->free_inode_count != 0)
+	const size_t bdgt_entry_count = data->get_bgdt_entry_count();
+	ext2_superblock& superblock = data->get_superblock();
+
+	for (size_t i = 0; i < bdgt_entry_count; i++)
+	{
+		auto bgd = &data->get_bgdt()[i];
+		if (bgd->free_inode_count == 0)
 		{
-			auto ret = ext2_block_read(fs, reinterpret_cast<uint8_t*>(bitmap_buf), bgd->block_bitmap_no);
-			if (ret != ERROR_SUCCESS)
-			{
-				delete[] bitmap_buf;
-				return ret;
-			}
+			continue;
+		}
 
-			ext2_superblock& superblock = data->get_superblock();
-			for (size_t j = 0; j < superblock.block_group_inode_count; j++)
+		if (auto ret = ext2_block_read(fs, reinterpret_cast<uint8_t*>(bitmap_bytes), bgd->block_bitmap_no);ret
+			!= ERROR_SUCCESS)
+		{
+			delete[] bitmap_buf;
+			return ret;
+		}
+
+		for (size_t j = 0; j < superblock.block_group_inode_count; j++)
+		{
+			if ((bitmap_buf[j / 64] & (1ull << (j % 64))) == 0) //free
 			{
-				if (0 == (bitmap_buf[j / 64] & (1u << (j % 64)))) //free
+				ext2_ino_type ino = EXT2_FIRST_INODE_NUMBER + i * superblock.block_group_inode_count + j;
+
+				bitmap_buf[j / 64] |= (1ull << (j % 64));
+
+				if (auto ret = ext2_block_write(fs, bitmap_bytes, bgd->block_bitmap_no);
+					ret != ERROR_SUCCESS)
 				{
-					ext2_ino_type ino = EXT2_FIRST_INODE_NUMBER + i * superblock.block_group_inode_count + j;
-
-					bitmap_buf[j / 64] |= (1ull << (j % 64));
-
-					ret = ext2_block_write(fs, reinterpret_cast<const uint8_t*>(bitmap_buf), bgd->block_bitmap_no);
-
-					if (ret != ERROR_SUCCESS)
-					{
-						delete[] bitmap_buf;
-						return ret;
-					}
-
-					bgd->free_inode_count--;
-					bgd->directory_count += is_dir;
-
-					size_t bgd_block_num = i * sizeof(ext2_block_group_desc) / data->get_block_size();
-
-					ret = ext2_block_write(fs,
-						reinterpret_cast<const uint8_t*>(data->get_bgdt()) + bgd_block_num * data->get_block_size(),
-						bgd->block_bitmap_no);
-
-					if (ret != ERROR_SUCCESS)
-					{
-						delete[] bitmap_buf;
-						return ret;
-					}
-
-					superblock.free_inode_count--;
-					ret = data->superblock_write_back(fs);
-
-					if (ret != ERROR_SUCCESS)
-					{
-						delete[] bitmap_buf;
-						return ret;
-					}
-
 					delete[] bitmap_buf;
-					return ino;
+					return ret;
 				}
+
+				bgd->free_inode_count--;
+				bgd->directory_count += is_dir;
+
+				size_t bgd_block_num = (i * sizeof(ext2_block_group_desc)) / data->get_block_size();
+
+				if (auto ret = ext2_block_write(fs,
+						reinterpret_cast<const uint8_t*>(data->get_bgdt()) + bgd_block_num * data->get_block_size(),
+						bgd_block_num + 2);ret != ERROR_SUCCESS)
+				{
+					delete[] bitmap_buf;
+					return ret;
+				}
+
+				superblock.free_inode_count--;
+
+				if (auto ret = data->superblock_write_back(fs);ret != ERROR_SUCCESS)
+				{
+					delete[] bitmap_buf;
+					return ret;
+				}
+
+				delete[] bitmap_buf;
+
+				return ino;
 			}
+
 		}
 	}
 
