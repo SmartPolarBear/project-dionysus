f560120cb22ff0ed3381a0d444a05d16eeeb4f53
SmartPolarBear <clevercoolbear@outlook.com>
2021-01-08 12:58:07 +0800

    Better arch-dependent register reading

--- arch/amd64/include/arch/amd64/cpu/regs.h
+++ arch/amd64/include/arch/amd64/cpu/regs.h
@@ -2,9 +2,10 @@
 
 #include "system/types.h"
 
-static inline uintptr_t rcr0() {
-	uintptr_t cr0;
-	asm volatile ("mov %%cr0, %0" : "=r" (cr0) :: "memory");
+static inline uintptr_t rcr0()
+{
+	uintptr_t cr0{ 0 };
+	asm volatile ("mov %%cr0, %0" : "=r" (cr0)::"memory");
 	return cr0;
 }
 
@@ -17,28 +18,30 @@ static inline void lcr0(uintptr_t val)
 
 static inline uintptr_t rcr2()
 {
-    uintptr_t val;
-    asm volatile("mov %%cr2,%0"
-                 : "=r"(val));
-    return val;
+	uintptr_t val{ 0 };
+	asm volatile("mov %%cr2,%0"
+	: "=r"(val));
+	return val;
 }
 
-static inline uintptr_t rcr3(void) {
-    uintptr_t cr3;
-    asm volatile ("mov %%cr3, %0" : "=r" (cr3) :: "memory");
-    return cr3;
+static inline uintptr_t rcr3()
+{
+	uintptr_t cr3{ 0 };
+	asm volatile ("mov %%cr3, %0" : "=r" (cr3)::"memory");
+	return cr3;
 }
 
 static inline void lcr3(uintptr_t val)
 {
-    asm volatile("mov %0,%%cr3"
-                 :
-                 : "r"(val));
+	asm volatile("mov %0,%%cr3"
+	:
+	: "r"(val));
 }
 
-static inline uintptr_t rcr4(void) {
-	uintptr_t cr4;
-	asm volatile ("mov %%cr4, %0" : "=r" (cr4) :: "memory");
+static inline uintptr_t rcr4()
+{
+	uintptr_t cr4{ 0 };
+	asm volatile ("mov %%cr4, %0" : "=r" (cr4)::"memory");
 	return cr4;
 }
 
@@ -49,13 +52,19 @@ static inline void lcr4(uintptr_t val)
 	: "r"(val));
 }
 
-
 //read eflags
-static inline size_t read_eflags(void)
+static inline uint32_t read_eflags()
 {
-    size_t eflags = 0;
-    asm volatile("pushf; pop %0"
-                 : "=r"(eflags));
-    return eflags;
+	uint64_t eflags{ 0 };
+	asm volatile("pushf; pop %0"
+	: "=r"(eflags));
+	return static_cast<uint32_t>(eflags & 0xFFFFFFFF);
 }
 
+static inline uint64_t read_rflags()
+{
+	uint64_t rflags{ 0 };
+	asm volatile("pushfq; pop %0"
+	: "=r"(rflags));
+	return rflags;
+}
