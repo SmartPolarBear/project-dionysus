5f07c7c51591fc780980e6548ab1720f3bdd524d
SmartPolarBear <clevercoolbear@outlook.com>
2021-01-31 14:05:26 +0800

    1. update[allocate_checker.hpp] use standard [[likely]] and [[unlikely]]

--- kern/libs/kbl/include/kbl/checker/allocate_checker.hpp
+++ kern/libs/kbl/include/kbl/checker/allocate_checker.hpp
@@ -8,52 +8,51 @@
 
 namespace kbl
 {
-	class allocate_checker final
+class allocate_checker final
+{
+ public:
+	allocate_checker() = default;
+
+	~allocate_checker()
 	{
-	 public:
-		allocate_checker() = default;
-
-		~allocate_checker()
+		if (armed) [[unlikely]]
 		{
-			if (unlikely(armed))
-			{
-				panic_unchecked();
-			}
+			panic_unchecked();
+		}
+	}
+
+	void arm(size_t size, bool res)
+	{
+		if (armed) [[unlikely]]
+		{
+			panic_doubly_armed();
 		}
 
-		void arm(size_t size, bool res)
-		{
-			if (unlikely(armed))
-			{
-				panic_doubly_armed();
-			}
+		armed = true;
+		ok = (size == 0 || res);
+	}
 
-			armed = true;
-			ok = (size == 0 || res);
-		}
+	bool check()
+	{
+		armed = false;
+		return ok;
+	}
 
-		bool check()
-		{
-			armed = false;
-			return likely(ok);
-		}
+ private:
+	bool armed = false;
+	bool ok = false;
 
-	 private:
-		bool armed = false;
-		bool ok = false;
+	[[noreturn]] static void panic_unchecked()
+	{
+		KDEBUG_GENERALPANIC("allocate_checker::check() isn't called");
+	}
 
-		[[noreturn]] static void panic_unchecked()
-		{
-			KDEBUG_GENERALPANIC("allocate_checker::check() isn't called");
-		}
-
-		[[noreturn]] static void panic_doubly_armed()
-		{
-			KDEBUG_GENERALPANIC("allocate_checker::arm() is called twice");
-		}
-	};
+	[[noreturn]] static void panic_doubly_armed()
+	{
+		KDEBUG_GENERALPANIC("allocate_checker::arm() is called twice");
+	}
+};
 }
-
 
 [[nodiscard]] inline void* operator new(size_t count, size_t flags, kbl::allocate_checker* ck) noexcept
 {
