27652117b3ad22eaea2ec85d4048c4a4920ba9a9
SmartPolarBear <clevercoolbear@outlook.com>
2021-02-12 00:17:50 +0800

    semaphore prototype

--- kern/libs/kbl/include/kbl/lock/semaphore.hpp
+++ kern/libs/kbl/include/kbl/lock/semaphore.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include "task/thread/thread.hpp"
+
 namespace kbl
 {
-class semaphore
-{
 
+class semaphore final
+{
+ public:
+	semaphore() = default;
+
+	semaphore(const semaphore&) = delete;
+	semaphore& operator=(const semaphore&) = delete;
+
+	void wait();
+
+	void signal();
+
+ private:
+	task::wait_queue wait_queue_{};
+
+	int64_t _count_{ 0 };
+	integral_atomic_ref<int64_t> count_{ _count_ };
 };
 }
---
+++ kern/libs/kbl/lock/semaphore.cc
@@ -0,0 +1,10 @@
+#include "kbl/lock/semaphore.hpp"
+
+void kbl::semaphore::wait()
+{
+
+}
+void kbl::semaphore::signal()
+{
+
+}
