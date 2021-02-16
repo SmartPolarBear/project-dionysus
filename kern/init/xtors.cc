75f538bf33e8fb0cb1f2a0039652191f411f7a37
SmartPolarBear <clevercoolbear@outlook.com>
2021-02-16 15:21:24 +0800

    format

--- kern/init/xtors.cc
+++ kern/init/xtors.cc
@@ -3,29 +3,31 @@
 
 extern "C"
 {
-    using ctor_type = void (*)();
-    using dtor_type = void (*)();
 
-    extern ctor_type start_ctors;
-    extern ctor_type end_ctors;
+using ctor_type = void (*)();
+using dtor_type = void (*)();
 
-    extern dtor_type start_dtors;
-    extern dtor_type end_dtors;
+extern ctor_type start_ctors;
+extern ctor_type end_ctors;
 
-    // IMPORTANT: initialization of libc components such as printf depends on this.
-    void call_ctors(void)
-    {
-        for (auto ctor = &start_ctors; ctor != &end_ctors; ctor++)
-        {
-            (*ctor)();
-        }
-    }
+extern dtor_type start_dtors;
+extern dtor_type end_dtors;
 
-    void call_dtors(void)
-    {
-        for (auto dtor = &start_dtors; dtor != &end_dtors; dtor++)
-        {
-            (*dtor)();
-        }
-    }
+// IMPORTANT: initialization of libc components such as printf depends on this.
+void call_ctors(void)
+{
+	for (auto ctor = &start_ctors; ctor != &end_ctors; ctor++)
+	{
+		(*ctor)();
+	}
+}
+
+void call_dtors(void)
+{
+	for (auto dtor = &start_dtors; dtor != &end_dtors; dtor++)
+	{
+		(*dtor)();
+	}
+}
+
 }
