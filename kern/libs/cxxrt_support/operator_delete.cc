eaf33e048fd054181bd319a0cb83cefa61bc3986
SmartPolarBear <clevercoolbear@outlook.com>
2021-02-23 13:56:53 +0800

    Add missing global operator delete s

--- kern/libs/cxxrt_support/operator_delete.cc
+++ kern/libs/cxxrt_support/operator_delete.cc
@@ -8,7 +8,17 @@ void operator delete(void* ptr) noexcept
 	memory::kfree(ptr);
 }
 
+void operator delete(void* ptr, std::align_val_t) noexcept
+{
+	memory::kfree(ptr);
+}
+
 void operator delete[](void* ptr) noexcept
+{
+	::operator delete(ptr);
+}
+
+void operator delete[](void* ptr, std::align_val_t) noexcept
 {
 	::operator delete(ptr);
 }
@@ -18,21 +28,33 @@ void operator delete(void* ptr, [[maybe_unused]] const std::nothrow_t& tag) noex
 	::operator delete(ptr);
 }
 
+void operator delete(void* ptr, std::align_val_t, [[maybe_unused]] const std::nothrow_t& tag) noexcept
+{
+	::operator delete(ptr);
+}
+
 void operator delete[](void* ptr, [[maybe_unused]] const std::nothrow_t& tag) noexcept
 {
 	::operator delete(ptr);
 }
 
+void operator delete[](void* ptr, std::align_val_t, [[maybe_unused]] const std::nothrow_t& tag) noexcept
+{
+	::operator delete(ptr);
+}
+
+
 // User-defined
 
-void operator delete(void* ptr, [[maybe_unused]] size_t flags)
-{
-	memory::kfree(ptr);
-}
 
-void operator delete[](void* ptr, [[maybe_unused]] size_t flags)
-{
-	::operator delete(ptr, flags);
-}
+//void operator delete(void* ptr, [[maybe_unused]] size_t flags)
+//{
+//	memory::kfree(ptr);
+//}
+//
+//void operator delete[](void* ptr, [[maybe_unused]] size_t flags)
+//{
+//	::operator delete(ptr, flags);
+//}
 
 // We use default placement delete in the std library
